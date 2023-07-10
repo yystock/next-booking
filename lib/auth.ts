f74cedf47300977efa592fcf2a993f15071bdefc
yystock <yystock5299@gmail.com>
2023-07-10 02:57:49 -0400

    trying to fix auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,11 @@
 import { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
 import GoogleProvider from "next-auth/providers/google";
 import TwitterProvider from "next-auth/providers/twitter";
 
 import { db } from "@/lib/db";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(db),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
