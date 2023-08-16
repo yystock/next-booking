d7482653f0f01c5193b278d7c0b798f6c6ec2181
yystock <yystock5299@gmail.com>
2023-08-15 22:07:08 -0400

    fixing bug1..

--- app/(admin)/dashboard/activity/components/client.tsx
+++ app/(admin)/dashboard/activity/components/client.tsx
@@ -7,9 +7,12 @@ import { Separator } from "@/components/ui/separator";
 import { useRouter } from "next/navigation";
 import { Activity } from "@prisma/client";
 import LoadMap from "@/components/map/load-map";
+import Map from "@/components/map/map";
 
+type ActivityType = Omit<Activity, "price"> & { price: string };
+type ActivitiesType = ActivityType[] | null;
 interface ActivityClientProps {
-  data: Activity[];
+  data: ActivitiesType;
 }
 
 export const ActivityClient: React.FC<ActivityClientProps> = ({ data }) => {
@@ -17,13 +20,15 @@ export const ActivityClient: React.FC<ActivityClientProps> = ({ data }) => {
   return (
     <>
       <div className="flex items-center justify-between">
-        <Heading title={`Activity (${data.length})`} description="Manage your activities" />
+        <Heading title={`Activity (${data && data.length})`} description="Manage your activities" />
         <Button onClick={() => router.push(`/dashboard/activity/new`)}>
           <Plus className="mr-2 h-4 w-4" /> Add New
         </Button>
       </div>
       <Separator />
-      <LoadMap initialData={data} />
+      <LoadMap>
+        <Map initialData={data} />
+      </LoadMap>
     </>
   );
 };
--- app/(admin)/dashboard/activity/page.tsx
+++ app/(admin)/dashboard/activity/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { db } from "@/lib/db";
 import { ActivityClient } from "./components/client";
 import { getCurrentUser } from "@/lib/session";
+import { formatter } from "@/lib/utils";
 
 const ActivityPage = async () => {
   const user = await getCurrentUser();
@@ -17,10 +18,17 @@ const ActivityPage = async () => {
     },
   });
 
+  const formattedActivities = activities.map((x) => {
+    return {
+      ...x,
+      price: formatter.format(x.price.toNumber()),
+    };
+  });
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <ActivityClient data={activities} />
+        <ActivityClient data={formattedActivities} />
       </div>
     </div>
   );
